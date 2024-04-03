3a9d84fe490526003d694f57a765575dc94aa3b7
EnzoFBS <enzoplay47@gmail.com>
2024-04-03 07:32:19 -0300

    Method post e get adicionado na api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
@@ -6,9 +7,31 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world"
 
-@app.route("/about")
-def about():
-    return "Página sobre"
+tasks = []
+tasks_id_control = 1
+
+@app.route('/tasks', methods=['POST'])
+def creat_task():
+    global tasks_id_control
+    data = request.get_json()
+    new_task = Task(id=1,title=data['title'], description=data.get("description", ""))
+    tasks_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+        "tasks": task_list, 
+        "total_tasks": len(task_list)
+  }
+    return jsonify(output)
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
